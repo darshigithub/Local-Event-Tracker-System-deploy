ef8ca57bdd98f137b73fb98cc12886e4b1dd4f94
darshigithub <darshanardarshanar@gmail.com>
2026-01-07 14:46:29 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to event organization app!"
+    return "Welcome to event organization app!!"
 
 if __name__ == '__main__':
     app.run(debug=True) 
