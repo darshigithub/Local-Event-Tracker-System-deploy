2faa4c7a0353ea78e562d6b9264e4a24543891df
Meghana-Sattigeri <meghanasattigeri13@gmail.com>
2026-01-08 11:03:16 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    print("✅ Tables created")
+    print("✅ Tables created yes")
 
 if __name__ == "__main__":
     app.run(debug=True)
