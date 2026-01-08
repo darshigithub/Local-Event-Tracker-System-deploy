56c8ba0fb8cd75ea05d332f1b9aa9a4f3dc9abe1
Arunkumar <arunanjan077@gmail.com>
2026-01-08 21:28:34 +0530

    frontend deleted

--- backend/app.py
+++ backend/app.py
@@ -1,45 +1,35 @@
 from flask import Flask
-from database.connection import db
 from flask_cors import CORS
+from database.connection import db
 
-
-
+from routes.user import user_bp
 from routes.booking import booking_bp
 from routes.review import review_bp
 
-from routes.user import user_bp
+app = Flask(__name__)
 
-<<<<<<< HEAD
+# Enable CORS
+CORS(app)
+
+# Database Configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Root1234@localhost:5432/event"
-=======
-app = Flask(__name__)   
-
-CORS(app)  
-
-app.register_blueprint(user_bp, url_prefix="/api")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:root@localhost:5432/event"
->>>>>>> c9ee61e82b0ec1bcbf24863b6c3d3937d39c8637
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Initialize DB
 db.init_app(app)
 
+# Register Blueprints
+app.register_blueprint(user_bp, url_prefix="/api")
 app.register_blueprint(booking_bp, url_prefix="/api")
 app.register_blueprint(review_bp, url_prefix="/api")
-
 
 @app.route("/")
 def home():
     return "Welcome to the Event Booking API"
 
+# Create tables
 with app.app_context():
     db.create_all()
-    
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(debug=True)
-
-=======
-    app.run(debug=True)
->>>>>>> c9ee61e82b0ec1bcbf24863b6c3d3937d39c8637
