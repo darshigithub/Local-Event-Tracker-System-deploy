3a70b7105d5dbe60b2f2be4e8984d9a13eb3bc4f
darshigithub <darshanardarshanar@gmail.com>
2026-01-11 10:32:08 +0530

    frontend deleted and backend updated

--- backend/database/event.sql
+++ backend/database/event.sql
@@ -12,8 +12,8 @@ CREATE TABLE events (
     capacity INT NOT NULL,
     price NUMERIC(10,2) NOT NULL DEFAULT 0,
  
-    latitude DECIMAL(9,6) NOT NULL,
-    longitude DECIMAL(9,6) NOT NULL,
+    latitude DECIMAL(9,6) NOT NULL, 
+    longitude DECIMAL(9,6) NOT NULL, 
     address TEXT,
  
     category VARCHAR(50),
@@ -22,5 +22,5 @@ CREATE TABLE events (
         CHECK (status IN ('active', 'cancelled', 'completed'))
         DEFAULT 'active',
  
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
