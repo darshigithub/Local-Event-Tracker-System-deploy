ec44785b6524779b381a0214478a6fc096a29b8a
darshigithub <darshanardarshanar@gmail.com>
2026-04-13 21:48:46 +0530

    jenkins deploy

--- docker/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Creating databases..."
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE event_management_db;
-    CREATE DATABASE inventory_db;
-EOSQL
---
+++ docker/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE event_management_db;
+CREATE DATABASE inventory_db;
