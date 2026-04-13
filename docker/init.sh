1c0777a9353b934411415b6fb0b01d40dc827744
darshigithub <darshanardarshanar@gmail.com>
2026-04-13 12:12:51 +0530

    database config for windows

--- docker/init.sh
+++ docker/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE event_management_db;
-    CREATE DATABASE inventory_db;
-EOSQL
+echo "Creating databases..."
+
+psql -U "$POSTGRES_USER" <<EOF
+CREATE DATABASE event_management_db;
+CREATE DATABASE inventory_db;
+EOF
