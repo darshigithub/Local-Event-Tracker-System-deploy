0ef08cfd40558a31852cf201253112edf70ef9d1
darshigithub <darshanardarshanar@gmail.com>
2026-04-14 16:15:22 +0530

    init updated

--- docker/init.sh
+++ docker/init.sh
@@ -4,6 +4,6 @@ set -e
 echo "Initializing PostgreSQL databases..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE $EVENT_DB;
-    CREATE DATABASE $INVENTORY_DB;
+    CREATE DATABASE event_management_db;
+    CREATE DATABASE inventory_db;
 EOSQL
