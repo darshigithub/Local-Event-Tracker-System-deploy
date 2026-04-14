4b9e40a65158585530f19c45eed06fe9aef8e7dc
darshigithub <darshanardarshanar@gmail.com>
2026-04-14 15:33:45 +0530

    init update

--- docker/init.sh
+++ docker/init.sh
@@ -4,27 +4,6 @@ set -e
 echo "Initializing PostgreSQL databases..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-
--- Create Event DB if not exists
-DO
-\$do\$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${EVENT_DB}') THEN
-      CREATE DATABASE "${EVENT_DB}";
-   END IF;
-END
-\$do\$;
-
--- Create Inventory DB if not exists
-DO
-\$do\$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${INVENTORY_DB}') THEN
-      CREATE DATABASE "${INVENTORY_DB}";
-   END IF;
-END
-\$do\$;
-
+    CREATE DATABASE $EVENT_DB;
+    CREATE DATABASE $INVENTORY_DB;
 EOSQL
-
-echo "Databases are ready!"
